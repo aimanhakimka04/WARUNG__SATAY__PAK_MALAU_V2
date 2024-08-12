2eeaeb55a7d65f52c3198d7fc862975f306254bf
AIMANSTUDENT04 <128442245+AIMANSTUDENT04@users.noreply.github.com>
2024-08-13 01:17:36 +0800

    Refactor database connection credentials in db_connect.php

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','pakmalausatay_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','Aiman@04hakim','pakmalausatay_db')or die("Could not connect to mysql".mysqli_error($con));
 
 
 
