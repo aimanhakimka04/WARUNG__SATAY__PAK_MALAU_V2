a1c687947464a8d38b3ba075e4a6314ae6b6cd96
AIMANSTUDENT04 <128442245+AIMANSTUDENT04@users.noreply.github.com>
2024-08-13 01:13:50 +0800

    Refactor database connection credentials in db_connection.php

--- testingchat/db_connection.php
+++ testingchat/db_connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn= new mysqli('localhost','root','Aiman@04Hakim','chatbot_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','Aiman@04hakim','chatbot_db')or die("Could not connect to mysql".mysqli_error($con));
 
 
 
